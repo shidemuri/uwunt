e120c9a1f17023bbd7dfd0350b99e0680de04580
teto lover <68622956+shidemuri@users.noreply.github.com>
2021-04-03 15:30:24 -0400

    Add files via upload

---
+++ lol.js
@@ -0,0 +1,30 @@
+const discord = require(`discord.js`)
+const client = new discord.Client()
+
+const lista = require(`./FODASE.json`)
+
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+const fuckmarkdown = require(`remove-markdown`)
+
+client.login(process.env.token)
+
+client.on(`ready`, () => {
+    console.log(`on`)
+    client.user.setActivity(`fuck uwu thats fag shit -padero/paradino`);
+})
+
+client.on(`message`, (m) => {
+    if(m.author.bot) return;
+    const args = m.content.slice(' ').toLowerCase();
+    lista.forEach(w =>{
+        if(fuckmarkdown(`${m.content}`).includes(w)){
+            if(m.deletable) m.delete()
+            m.channel.send(`${m.author} fuck off`).then(ww =>{
+                sleep(2500).then(() => {
+                    ww.delete()
+                });
+            })
+        }
+    })
+})
