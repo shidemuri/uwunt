406b210b8405b8c2fc8ff416f84326ed9ec8e2c4
teto lover <68622956+shidemuri@users.noreply.github.com>
2021-04-06 20:35:00 -0400

    Update lol.js

--- lol.js
+++ lol.js
@@ -15,22 +15,12 @@ client.on(`ready`, () => {
     client.user.setActivity(`fuck uwu thats fag shit -padero/paradino`);
 })
 
-/*client.on("guildMemberAdd", member => {
-  if (Date.now() - member.user.createdAt < 1000*60*60*24*10) {
-    if(Date.now() - member.user.createdAt < 1000*60*60*24*3){
-        client.channels.cache.get(`827493108379942955`).send(`${member.id} less than 3 days old 100% sus`)
-    } else {
-        client.channels.cache.get(`827493108379942955`).send(`${member.id} less than 10 days old kinda sus`)
-    }
-  }
-});
-*/
 client.on(`message`, (m) => {
     if(m.author.bot) return;
     lista.forEach(w =>{
-        if(fuckmarkdown(`${m.content.toLowerCase().split(" ").join("")}`).includes(w)){
+        if(fuckmarkdown(`${m.content.toLowerCase()}`).includes(w)){
             if(m.deletable) m.delete()
-            let e = Math.floor(Math.random() * lolw.length)
+            let e = Math.floor(Math.random() * lolw.length) //random message
             m.channel.send(`${m.author} ${lolw[e]}`).then(ww =>{
                 sleep(2500).then(() => {
                     ww.delete()
